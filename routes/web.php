297e8e46413f219b5eff6617f9df7f7f7dd61631
= <=>
2022-03-09 10:50:05 +0700

    Make seeder and migrate

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\aboutController;
 use App\Http\Controllers\contactController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
